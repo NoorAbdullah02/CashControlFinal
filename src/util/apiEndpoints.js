7f9cb6f2ca0bcbb9c9f49a58684ffd3ee9320c50
noorabdullah02 <sheikhnoorabdullah02@gmail.com>
2025-08-20 22:37:24 +0600

    Server

--- src/util/apiEndpoints.js
+++ src/util/apiEndpoints.js
@@ -1,6 +1,6 @@
-export const BASE_URL = "http://localhost:8080/api/v1.0";
+// export const BASE_URL = "http://localhost:8080/api/v1.0";
 
-// export const BASE_URL = "https://cash-control-api.onrender.com/api/v1.0";
+export const BASE_URL = "https://cash-control-api.onrender.com/api/v1.0";
 
 
 const CLOUDINARY_CLOUD_NAME = "dhadf5h7j";
