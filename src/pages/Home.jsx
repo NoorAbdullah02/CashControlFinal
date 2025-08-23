b05f92ab6f0eb989c5b66dca0eda10944c3fd65d
noorabdullah02 <sheikhnoorabdullah02@gmail.com>
2025-08-23 22:17:14 +0600

    New

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -55,7 +55,8 @@ const Home = () => {
         const hour = currentTime.getHours();
         if (hour < 12) return "Good Morning";
         if (hour < 17) return "Good Afternoon";
-        return "Good Evening";
+        if (hour < 21) return "Good Evening";
+        return "Good Night";
     };
 
     const formatDate = (date) => {
